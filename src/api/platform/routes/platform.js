e6f70f1c3142ed80ac570f3d5726df9bc52d2677
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2026-08-30 13:28:46 +0600

    admin can delete: logic building

--- src/api/platform/routes/platform.js
+++ src/api/platform/routes/platform.js
@@ -1,11 +1,37 @@
-'use strict';
-const ADMIN_ONLY = { name: 'global::has-role', config: { roles: ['admin'] } };
+"use strict";
+const ADMIN_ONLY = { name: "global::has-role", config: { roles: ["admin"] } };
 
 module.exports = {
   routes: [
-    { method: 'GET', path: '/platform/users', handler: 'platform.listUsers', config: { policies: ['global::is-authenticated', ADMIN_ONLY] } },
-    { method: 'PUT', path: '/platform/users/:id/role', handler: 'platform.updateUserRole', config: { policies: ['global::is-authenticated', ADMIN_ONLY] } },
-    { method: 'GET', path: '/platform/roles', handler: 'platform.listRoles', config: { policies: ['global::is-authenticated', ADMIN_ONLY] } },
-    { method: 'GET', path: '/platform/stats', handler: 'platform.stats', config: { policies: ['global::is-authenticated', ADMIN_ONLY] } },
+    {
+      method: "GET",
+      path: "/platform/users",
+      handler: "platform.listUsers",
+      config: { policies: ["global::is-authenticated", ADMIN_ONLY] },
+    },
+    {
+      method: "PUT",
+      path: "/platform/users/:id/role",
+      handler: "platform.updateUserRole",
+      config: { policies: ["global::is-authenticated", ADMIN_ONLY] },
+    },
+    {
+      method: "DELETE",
+      path: "/platform/users/:id",
+      handler: "platform.deleteUser",
+      config: { policies: ["global::is-authenticated", ADMIN_ONLY] },
+    },
+    {
+      method: "GET",
+      path: "/platform/roles",
+      handler: "platform.listRoles",
+      config: { policies: ["global::is-authenticated", ADMIN_ONLY] },
+    },
+    {
+      method: "GET",
+      path: "/platform/stats",
+      handler: "platform.stats",
+      config: { policies: ["global::is-authenticated", ADMIN_ONLY] },
+    },
   ],
 };
